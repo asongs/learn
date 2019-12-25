38623eb67121d13433af0814f0e8e4bb4412c4a2
tangsong <790947779@qq.com>
2019-12-25 18:01:29 +0800

    test

---
+++ nacos-demo/src/main/java/com/asong/nacosdemo/NacosDemoApplication.java
@@ -0,0 +1,18 @@
+package com.asong.nacosdemo;
+
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Asong
+ */
+@SpringBootApplication
+@NacosPropertySource(dataId = "example", autoRefreshed = true)
+public class NacosDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDemoApplication.class, args);
+    }
+
+}
---
+++ thread-demo/src/main/java/com/asong/threaddemo/ThreadDemoApplication.java
@@ -0,0 +1,16 @@
+package com.asong.threaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Asong
+ */
+@SpringBootApplication
+public class ThreadDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadDemoApplication.class, args);
+    }
+
+}
