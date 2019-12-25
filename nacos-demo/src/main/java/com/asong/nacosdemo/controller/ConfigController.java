c110202860943ab4665704496da9653c3fd44c9e
tangsong <790947779@qq.com>
2019-12-25 17:57:49 +0800

    test

---
+++ nacos-demo/src/main/java/com/asong/nacosdemo/controller/ConfigController.java
@@ -0,0 +1,27 @@
+package com.asong.nacosdemo.controller;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * @author Asong
+ * @title: ConfigController
+ * @date 2019-09-26 15:11
+ */
+
+@Controller
+@RequestMapping("config")
+public class ConfigController {
+
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @RequestMapping(value = "/get", method = GET)
+    @ResponseBody
+    public boolean get() {
+        return useLocalCache;
+    }
+}
---
+++ thread-demo/src/main/java/com/asong/threaddemo/basic/ThreadStatusDemo.java
@@ -0,0 +1,14 @@
+package com.asong.threaddemo.basic;
+
+/**
+ * @author Asong
+ * @title: ThreadStatusDemo
+ * @date 2019-09-29 17:37
+ */
+public class ThreadStatusDemo {
+
+
+    public static void main(String[] args) {
+
+    }
+}
